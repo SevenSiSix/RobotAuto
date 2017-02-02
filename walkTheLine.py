5e6a479b4f79bcd0dddd91ba83886968118ed475
KarhuMajor <Tim@smit-hoen.nl>
2017-02-02 14:44:42 +0100

    Update walkTheLine.py

--- walkTheLine.py
+++ walkTheLine.py
@@ -31,12 +31,12 @@ pinLED2 = 23
 #----------------------------------------------------------
 
 '''FREQUENTIE VAN DE WIELEN, HOEVAAK AAN/UIT PER SECONDE'''
-frequentie = 20
+frequentie = 25
 #---------------------------------------------------------
 
 '''PROCENTEN DAT DE WIELEN AAN MOETEN STAAN'''
 rondjesLinks = 30
-rondjesRechts = 45
+rondjesRechts = 40
 #---------------------------------------------------------
 
 '''VARIABELE VOOR HET STOPPEN VAN DE AUTO'''

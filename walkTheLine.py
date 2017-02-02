c9198a395a4d9571a1f47c09d9e3ad629d2e032f
KarhuMajor <Tim@smit-hoen.nl>
2017-02-02 14:54:10 +0100

    Update walkTheLine.py

--- walkTheLine.py
+++ walkTheLine.py
@@ -132,7 +132,7 @@ try:
 		#Als de sensor geen licht oppikt is het oppervlak zwart
 		if GPIO.input(pinCheckLijn)==0:
 			print('De sensor staat boven een zwart oppervlak')
-			HalfRechts()
+			Rechts()
 		#Als de sensor wel iets oppikt (dus 1 geeft) is het een wit oppervlak
 		else:
 			print('De sensor staat boven een wit oppvervlak')

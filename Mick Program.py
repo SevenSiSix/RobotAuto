0acd778a7ca28651a1705ec2093712364ca4f0c4
SevenSiSix <mickbeugelsdijk@gmail.com>
2017-02-02 14:30:46 +0100

    a

--- Mick Program.py
+++ Mick Program.py
@@ -142,7 +142,7 @@ try:
     while True:
         while isObstakel(hoeDichtBij):
             Links()
-            time.sleep(0.28)
+            time.sleep(0.2)
             print "Naar Links"
 
         rijVooruit()
